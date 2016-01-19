190f77126878bb14e8e653ab205b4c0ee510d16a
hanggao121 <hang_gao@amway.com>
2016-01-19 12:34:12 +0800

    static resource

--- src/main/java/com/kkt/framework/config/MvcWebConfig.java
+++ src/main/java/com/kkt/framework/config/MvcWebConfig.java
@@ -1,16 +1,20 @@
 package com.kkt.framework.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.kkt.*.controller, com.kkt.*.webservice, com.kkt.*.service")
 public class MvcWebConfig extends WebMvcConfigurerAdapter {
-    
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
@@ -19,4 +23,19 @@ public class MvcWebConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/login").setViewName("login");
     }
 
+    @Bean
+    public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
+        return new ResourceUrlEncodingFilter();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        VersionResourceResolver versionResolver = new VersionResourceResolver()
+                .addContentVersionStrategy("/css/**", "/js/**");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:static/")
+                .setCachePeriod(null)
+                .resourceChain(true)
+                .addResolver(versionResolver);
+    }
 }
